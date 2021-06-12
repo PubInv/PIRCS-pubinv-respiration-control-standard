4e26e503d230699214c1cbcab2c83fe7338ed95b
Robert L. Read <read.robert@gmail.com>
2021-06-12 18:13:27 -0500

    moving macros into .cpp to prevent colliding definition

--- pircs_library/PIRCS.cpp
+++ pircs_library/PIRCS.cpp
@@ -50,6 +50,18 @@ as a C file.
 #include "PIRCS.h"
 
 
+#ifndef htons
+
+#define htons(x) ( (((x)<<8)&0xFF00) | (((x)>>8)&0xFF) )
+#define ntohs(x) htons(x)
+
+#define htonl(x) ( ((x)<<24 & 0xFF000000UL) | \
+                   ((x)<< 8 & 0x00FF0000UL) | \
+                   ((x)>> 8 & 0x0000FF00UL) | \
+                   ((x)>>24 & 0x000000FFUL) )
+#define ntohl(x) htonl(x)
+
+#endif // !defined(htons)
 
 
 // From: https://stackoverflow.com/questions/122616/how-do-i-trim-leading-trailing-whitespace-in-a-standard-way
--- pircs_library/PIRCS.h
+++ pircs_library/PIRCS.h
@@ -44,18 +44,6 @@ SOFTWARE.
 #include <inttypes.h>
 
 
-#ifndef htons
-
-#define htons(x) ( (((x)<<8)&0xFF00) | (((x)>>8)&0xFF) )
-#define ntohs(x) htons(x)
-
-#define htonl(x) ( ((x)<<24 & 0xFF000000UL) | \
-                   ((x)<< 8 & 0x00FF0000UL) | \
-                   ((x)>> 8 & 0x0000FF00UL) | \
-                   ((x)>>24 & 0x000000FFUL) )
-#define ntohl(x) htonl(x)
-
-#endif // !defined(htons)
 
 // struct containing SetCoommandValues (fixed size 7 bytes)
 typedef struct SetCommand
